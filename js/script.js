abaacbb31f07732715e5cd4e9a0071abe5082120
MiLee777 <midyashka1506@gmail.com>
2024-05-24 21:40:22 +0700

    feat: add About me section

--- js/script.js
+++ js/script.js
@@ -9,3 +9,34 @@ function hambHandler(e) {
   menu.classList.toggle('open');
   hamb.classList.toggle('active');
 }
+
+
+// About
+
+document.addEventListener('DOMContentLoaded', () => {
+  const infoElements = document.querySelectorAll('#info');
+  const buttons = document.querySelectorAll('#about__btn');
+
+  function changeInfo(index) {
+    infoElements.forEach((element) => {
+      element.classList.remove('info__show');
+    });
+    buttons.forEach((button) => {
+      button.classList.remove('btn__info_active');
+    });
+    if (infoElements[index] && buttons[index]) {
+      infoElements[index].classList.add('info__show');
+      buttons[index].classList.add('btn__info_active');
+    }
+  }
+
+  buttons.forEach((button, index) => {
+    button.addEventListener('click', () => changeInfo(index));
+  });
+});
+
+
+
+
+
+
