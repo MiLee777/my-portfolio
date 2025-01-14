be74c0792f5762e7c890a76b3eeb8caade289eb1
MiLee777 <midyashka1506@gmail.com>
2025-01-14 23:22:18 +0700

    feat: add notes and nutrition analyzes web sites

--- js/dataProject.js
+++ js/dataProject.js
@@ -1,5 +1,13 @@
 const dataProject = [
   {
+    title: 'Coffee House',
+    subtitle: 'HTML, CSS, JavaScript, React, Redux',
+    description: 'This project is a simple web application for a coffee shop menu. It allows users to browse through various coffee drinks, view their details, and add them to an order list. Users can also remove items from the order list and clear the entire list if needed.',
+    image: './assets/portfolio/coffee-house.jpg',
+    src: 'https://milee777-coffee-house.netlify.app/'
+  },
+
+  {
     title: 'Momentum App',
     subtitle: 'HTML, CSS, JavaScript',
     description: 'This web application is a comprehensive tool combining various features, including a clock and calendar, a slider with background images, a weather widget, a quote of the day, and an audio player. Each component offers a unique functionality to enhance the user experience.',
@@ -9,24 +17,32 @@ const dataProject = [
 
   {
     title: 'Cozy House (shelter)',
-    subtitle: 'HTML, CSS, JavaScript',
+    subtitle: 'HTML, CSS, JavaScript, GSAP',
     description: 'This is a website for the "Cozy House" shelter, providing information about the shelter, pets available for adoption, ways to help the shelter, and contact details. The website features a responsive design, a modal popup for pet details, and a hamburger menu for easy navigation on mobile devices.',
     image: './assets/portfolio/cozy-house.jpg',
     src: 'https://rolling-scopes-school.github.io/milee777-JSFEPRESCHOOL2024Q2/shelter/'
   },
 
   {
-    title: 'Coffee House',
-    subtitle: 'HTML, CSS, JavaScript, React, Redux',
-    description: 'This project is a simple web application for a coffee shop menu. It allows users to browse through various coffee drinks, view their details, and add them to an order list. Users can also remove items from the order list and clear the entire list if needed.',
-    image: './assets/portfolio/coffee-house.jpg',
-    src: 'https://milee777-coffee-house.netlify.app/'
+    title: 'Notes',
+    subtitle: 'HTML, CSS, JavaScript, React, Node.js, MongoDB',
+    description: 'The notes website is an application designed for creating, storing, and managing notes. Users can easily add, edit, and delete notes.',
+    image: './assets/portfolio/notes.jpg',
+    src: 'https://milee777-notes.netlify.app/'
+  },
+
+  {
+    title: 'Nutrition Analysis',
+    subtitle: 'HTML, CSS, React, API',
+    description: 'This is a simple web application for analyzing nutrition data of various food products. The app allows users to enter the quantity, unit, and name of a food product, and then fetches the nutrition data for that product from the Edamam API.',
+    image: './assets/portfolio/nutrition.jpg',
+    src: 'https://milee777-nutrition-analysis-app.netlify.app/'
   },
 
   {
     title: 'Shelter',
     subtitle: 'HTML, CSS, Bootstrap',
-    description: 'Welcome to the "Happy Place" Shelter web application! This application is dedicated to promoting and supporting our shelter, where we care for and find loving homes for our animals. It includes sections about the shelter, how to choose a friend, ways to help, and how to become a volunteer. ',
+    description: 'Welcome to the "Happy Place" Shelter web application! This application is dedicated to promoting and supporting our shelter, where we care for and find loving homes for our animals. It includes sections about the shelter, how to choose a friend, ways to help, and how to become a volunteer.',
     image: './assets/portfolio/happy-place.jpg',
     src: 'https://ambitious-wholesale-chopper.glitch.me'
   },
